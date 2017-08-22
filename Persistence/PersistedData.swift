56f6f62e69f967825ef98747fe8425033e1dbbbc
Csaba Babos <babos.csaba@yourweb.hu>
2017-08-22 17:11:50 +0200

    Added the project

--- Persistence/PersistedData.swift
+++ Persistence/PersistedData.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import os.log
+
+class PersistedData: NSObject, NSCoding {
+  public var date: Date
+  private var text: String
+  
+  static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+  static let ArchiveURL = DocumentsDirectory.appendingPathComponent("persistedData")
+  
+  override init() {
+    self.text = ""
+    self.date = Date()
+    super.init()
+  }
+  
+  init(text: String, date: Date) {
+    self.text = text
+    self.date = date
+  }
+  
+  func setStored(text: String) {
+    self.text = text;
+    self.date = Date();
+  }
+  
+  func getStored() -> String {
+    os_log("Saved at: %s", date.description)
+    return text
+  }
+  
+  //MARK: NSCoding
+  func encode(with aCoder: NSCoder) {
+    aCoder.encode(text, forKey: "text")
+    aCoder.encode(date, forKey: "date")
+  }
+  
+  required convenience init?(coder aDecoder: NSCoder) {
+    guard let loadedText = aDecoder.decodeObject(forKey: "text") as? String
+      else {
+        os_log("Could not decode text")
+        return nil
+      }
+    
+    guard let loadedDate = aDecoder.decodeObject(forKey: "date") as? Date
+      else {
+        os_log("Could not decode text")
+        return nil
+      }
+    
+    self.init(text: loadedText, date: loadedDate)
+  }
+  
+}
--- Persistence/ViewController.swift
+++ Persistence/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import os.log
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var TextInput: UITextField!
+  @IBOutlet weak var SavedTextField: UITextView!
+  private var persistedData = PersistedData();
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    if let loadedData = loadPersistedData() {
+      persistedData = loadedData;
+      setShowedText(to: persistedData.getStored())
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func SaveNewInput(_ sender: Any) {
+    let newInput = getNewText();
+    setShowedText(to: newInput);
+    persistedData.setStored(text: newInput)
+    persistData()
+  }
+  
+  func getNewText() -> String {
+    var newText = "";
+    
+    if let returnText = TextInput.text {
+      newText = returnText;
+    }
+    
+    return newText;
+  }
+  
+  func setShowedText(to text: String) {
+    SavedTextField.text = text + " " + persistedData.date.description;
+  }
 
+  //MARK: Persistence 
+  private func persistData() {
+    let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(persistedData, toFile: PersistedData.ArchiveURL.path)
+    if isSuccessfulSave {
+      os_log("Data successfully saved.", log: .default, type: .debug)
+    } else {
+      os_log("Failed to save data...", log: .default, type: .error)
+    }
+  }
+  
+  private func loadPersistedData() -> PersistedData?  {
+    return NSKeyedUnarchiver.unarchiveObject(withFile: PersistedData.ArchiveURL.path) as? PersistedData
+  }
 
 }
 
